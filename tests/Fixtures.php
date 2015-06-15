8cf0ef8a6a6b64ee3f1c73a9a26a1f7302084fff
Raphael Antonmattei <rantonmattei@theorchard.com>
2015-06-15 11:18:37 -0400

    Added fixtures

---
+++ tests/Fixtures.php
@@ -0,0 +1,118 @@
+<?php
+namespace Cascade\Tests;
+
+class Fixtures
+{
+    /**
+     * Return the fixture directory
+     * @return string ficture directory
+     */
+    public static function fixtureDir()
+    {
+        return realpath(__DIR__.'/Fixtures');
+    }
+
+    /**
+     * Return a path to a non existing file
+     * @return string wrong file path
+     */
+    public static function getInvalidFile()
+    {
+        return 'some/non/existing/file.txt';
+    }
+
+    /**
+     * Return the fixture Yaml config file
+     * @return string path to yaml config file
+     */
+    public static function getYamlConfigFile()
+    {
+        return self::fixtureDir().'/fixture_config.yml';
+    }
+
+    /**
+     * Return the fixture sample Yaml file
+     * @return string path to a sample yaml file
+     */
+    public static function getSampleYamlFile()
+    {
+        return self::fixtureDir().'/fixture_sample.yml';
+    }
+
+    /**
+     * Return the fixture sample Yaml string
+     * @return string sample yaml string
+     */
+    public static function getSampleYamlString()
+    {
+        return trim(
+            '---'."\n".
+            'greeting: "hello"'."\n".
+            'to: "you"'."\n"
+        );
+    }
+
+    /**
+     * Return the fixture JSON config file
+     * @return string path to JSON config file
+     */
+    public static function getJsonConfigFile()
+    {
+        return self::fixtureDir().'/fixture_config.json';
+    }
+
+    /**
+     * Return the fixture sample JSON file
+     * @return string path to a sample JSON file
+     */
+    public static function getSampleJsonFile()
+    {
+        return self::fixtureDir().'/fixture_sample.json';
+    }
+
+    /**
+     * Return the fixture sample JSON string
+     * @return string sample JSON string
+     */
+    public static function getSampleJsonString()
+    {
+        return trim(
+            '{'."\n".
+            '    "greeting": "hello",'."\n".
+            '    "to": "you"'."\n".
+            '}'."\n"
+        );
+    }
+
+    /**
+     * Return a sample string
+     * @return string sample string
+     */
+    public static function getSampleString()
+    {
+        return " some string with new \n\n lines and white spaces \n\n";
+    }
+
+    /**
+     * Return a config array
+     * @return array config array
+     */
+    public static function getPhpArrayConfig()
+    {
+        require self::fixtureDir().'/fixture_config.php';
+
+        return $fixtureArray;
+    }
+
+    /**
+     * Return a sample array
+     * @return array sample array
+     */
+    public static function getSamplePhpArray()
+    {
+        return array(
+            'greeting' => 'hello',
+            'to' => 'you'
+        );
+    }
+}
---
+++ tests/Fixtures/fixture_config.php
@@ -0,0 +1,45 @@
+<?php
+
+$fixtureArray = array(
+    'version' => 1,
+
+    'formatters' => array(
+        'spaced' => array(
+            'format' => "%datetime% %channel%.%level_name%  %message%\n",
+            'include_stacktraces' => true
+            # dummy_option: "heyhey"
+            # date_format: "Y/m/d"
+        ),
+        'dashed' => array(
+            'format' => "%datetime%-%channel%.%level_name% - %message%\n"
+        ),
+    ),
+    'handlers' => array(
+        'console' => array(
+            'class' => 'Monolog\Handler\StreamHandler',
+            'level' => 'DEBUG',
+            'formatter' => 'spaced',
+            'stream' => 'php://stdout'
+        ),
+
+        'info_file_handler' => array(
+            'class' => 'Monolog\Handler\StreamHandler',
+            'level' => 'INFO',
+            'formatter' => 'dashed',
+            'stream' => './demo_info.log'
+        ),
+
+        'error_file_handler' => array(
+            'class' => 'Monolog\Handler\StreamHandler',
+            'level' => 'ERROR',
+            'stream' => './demo_error.log',
+            'formatter' => 'spaced'
+        )
+    ),
+    'processors' => array(),
+    'loggers' => array(
+        'my_logger' => array(
+            'handlers' => array('console', 'info_file_handler')
+        )
+    )
+);
