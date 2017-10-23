0c335ccda050c2133730e7ffd75ea2c03d69f6ae
Laurent Laville <laurent.laville@gmail.com>
2017-10-23 21:21:56 +0200

    Make mock getter compatible PHPUnit 4.8 and 5.7

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,24 @@ namespace Cascade\Tests;
 
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
+    public function getMock($originalClassName, $methods = [], array $arguments = [], $mockClassName = '', $callOriginalConstructor = true, $callOriginalClone = true, $callAutoload = true, $cloneArguments = false, $callOriginalMethods = false, $proxyTarget = null)
+    {
+        $phpUnitVersion = \PHPUnit_Runner_Version::id();
+
+        if (version_compare($phpUnitVersion, '5.7.0', 'lt')) {
+            return parent::getMock(
+                $originalClassName,
+                $methods,
+                $arguments,
+                $mockClassName,
+                $callOriginalConstructor,
+                $callOriginalClone,
+                $callAutoload,
+                $cloneArguments,
+                $callOriginalMethods,
+                $proxyTarget
+            );
+        }
+        return $this->createMock($originalClassName);
+    }
 }
