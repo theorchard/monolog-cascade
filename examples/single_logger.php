f7d80768b465389c02f53df98fb048ebb253ae47
Zhwt <greatzhwt@gmail.com>
2019-10-15 11:21:26 +0800

    fix typo "following"

--- examples/single_logger.php
+++ examples/single_logger.php
@@ -7,5 +7,5 @@ $logger = Cascade::getLogger('some_logger');
 $logger->pushHandler(new Monolog\Handler\StreamHandler('php://stdout'));
 $logger->info('Hellooooo World!');
 
-// you should see the follwing in the stdout:
+// you should see the following in the stdout:
 //    [YYYY-mm-dd hh:mm:ss] some_logger.INFO: Hellooooo World!
